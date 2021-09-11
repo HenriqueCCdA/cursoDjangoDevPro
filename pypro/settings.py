cd02fea1bd9d026dff2115672b958aae85a3fa44
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2021-09-11 00:28:54 -0300

    I5 (#7)
    * Criados os arquivos para o deploy automatico no heroku.
    
    close #5

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!$^jardbc8*hym)htsyplbt$l_fu7yg=2%oit%0eq*^_js+$kb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

df5105990332bc0b734872c615e0cd09f60443b8
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-10-11 00:19:40 -0300

    Acesso a Relação com Atributo _set
    close #146

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -5,6 +5,5 @@ from pypro.modulos import facade
 
 def detalhe(request, slug):
     modulo = facade.encontar_modulo(slug)
-    print(slug, modulo)
-
-    return render(request, 'modulos/modulo_detalhe.html', context={'modulo': modulo})
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', context={'modulo': modulo, 'aulas': aulas})
